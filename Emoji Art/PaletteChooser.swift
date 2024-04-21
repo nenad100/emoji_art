f6905e83d10cb5a8adc26f5918f5db0ba77325df
nenad100 <nenadstojanov84@gmail.com>
2024-04-21 22:47:11 +0200

    Lecture 12 - save EmojiArt to file system (use Data and URL) - use UserDefaults to store palettes state Lecture 13 - Go To menu - PaletteEditor

--- Emoji Art/PaletteChooser.swift
+++ Emoji Art/PaletteChooser.swift
@@ -10,12 +10,17 @@ import SwiftUI
 struct PaletteChooser: View {
     @EnvironmentObject var store: PaletteStore
     
+    @State private var showPaletteEditor = false
+    
     var body: some View {
         HStack {
             chooser
             view(for: store.palettes[store.cursorIndex])
         }
         .clipped()
+        .sheet(isPresented: $showPaletteEditor) {
+            PaletteEditor()
+        }
     }
     
     var chooser: some View {
@@ -23,12 +28,30 @@ struct PaletteChooser: View {
             store.cursorIndex += 1
         }
         .contextMenu{
+            gotoMenu
             AnimatedActionButton("New", systemImage: "plus") {
                 store.insert(name: "Math", emojis: "+−×÷∝∞")
             }
             AnimatedActionButton("Delete", systemImage: "minus.circle", role: .destructive) {
                 store.palettes.remove(at: store.cursorIndex)
             }
+            AnimatedActionButton("Edit", systemImage: "pencil") {
+                showPaletteEditor = true
+            }
+        }
+    }
+    
+    private var gotoMenu: some View {
+        Menu {
+            ForEach(store.palettes) { palette in
+                AnimatedActionButton(palette.name) {
+                    if let index = store.palettes.firstIndex(where: { $0.id == palette.id }) {
+                        store.cursorIndex = index
+                    }
+                }
+            }
+        } label: {
+            Label("Go To", systemImage: "text.insert")
         }
     }
     
---
+++ Emoji Art/PaletteEditor.swift
@@ -0,0 +1,18 @@
+//
+//  PaletteEditor.swift
+//  Emoji Art
+//
+//  Created by Nenad Stojanov on 21.4.24.
+//
+
+import SwiftUI
+
+struct PaletteEditor: View {
+    var body: some View {
+        Text("Palette Editor")
+    }
+}
+
+#Preview {
+    PaletteEditor()
+}
