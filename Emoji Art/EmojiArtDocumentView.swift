d3b0f5ca93a8a9a3d514f55d09795b499744f5ca
nenad100 <nenadstojanov84@gmail.com>
2024-04-27 08:08:24 +0200

    Lecture 14 - Added state machine Background for status of fetching background images - Asynchronously fetch background images

--- Emoji Art/EmojiArtDocumentView.swift
+++ Emoji Art/EmojiArtDocumentView.swift
@@ -31,6 +31,15 @@ struct EmojiArtDocumentView: View {
         GeometryReader { geometry in
             ZStack {
                 Color.white
+                if document.background.isFetching {
+                    ProgressView()
+                        .scaleEffect(2)
+                        .tint(.blue)
+                        .position(
+                            Emoji.Position.zero.in(geometry)
+                        )
+                    
+                }
                 documentContents(in: geometry)
                     .scaleEffect(zoom * gestureZoom)
                     .offset(pan + gesturePan)
@@ -39,8 +48,46 @@ struct EmojiArtDocumentView: View {
             .dropDestination(for: Sturldata.self) { sturlDatas, location in
                 return drop(sturlDatas, at: location, in:geometry)
             }
+            .onChange(of: document.background.failureReason) { reason in
+                showBackgroundFailureAlert = reason != nil
+            }
+            .onChange(of: document.background.uiImage) { uiImage in
+                zoomToFit(uiImage?.size, in: geometry)
+            }
+            .alert("Set Background",
+                   isPresented: $showBackgroundFailureAlert,
+                   presenting: document.background.failureReason,
+                   actions: { reason in
+                Button("OK", role: .cancel) {}
+            },
+                   message: { reason in
+                Text (reason)
+            }
+            )
         }
     }
+    
+    private func zoomToFit(_ size: CGSize?, in geometry: GeometryProxy) {
+        if let size {
+            zoomToFit(CGRect(center: .zero, size: size), in: geometry)
+        }
+    }
+    
+    private func zoomToFit(_ rect: CGRect, in geometry: GeometryProxy) {
+        withAnimation {
+            if rect.size.width > 0, rect.size.height > 0, geometry.size.width > 0, geometry.size.height > 0 {
+                let hZoom = geometry.size.width / rect.size.width
+                let vZoom = geometry.size.height / rect.size.height
+                zoom = min (hZoom, vZoom)
+                pan = CGOffset(
+                    width: -rect.minX * zoom,
+                    height: -rect.minY * zoom
+                )
+            }
+        }
+    }
+    
+    @State private var showBackgroundFailureAlert = false
     
     @State private var zoom: CGFloat = 1
     @State private var pan: CGOffset = .zero
@@ -70,18 +117,10 @@ struct EmojiArtDocumentView: View {
     
     @ViewBuilder
     private func documentContents(in geometry: GeometryProxy) -> some View {
-        AsyncImage(url: document.background) { phase in
-            if let image = phase.image {
-                image
-            } else if let url = document.background {
-                if phase.error != nil {
-                    Text("\(url)")
-                } else {
-                    ProgressView()
-                }
-            }
+        if let uiImage = document.background.uiImage {
+            Image(uiImage: uiImage)
+                .position(Emoji.Position.zero.in(geometry))
         }
-            .position(Emoji.Position.zero.in(geometry))
         ForEach(document.emojis) { emoji in
             Text(emoji.string)
                 .font(emoji.font)
